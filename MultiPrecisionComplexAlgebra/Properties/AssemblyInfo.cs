384e2426e1fff21f179dec7320757431dc0fdcff
tk-yoshimura <yoshimura8192@gmail.com>
2024-09-01 04:25:16 +0900

    Update AssemblyInfo.cs

--- MultiPrecisionComplexAlgebra/Properties/AssemblyInfo.cs
+++ MultiPrecisionComplexAlgebra/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("95CE4B11-C3A1-4D54-9849-1C4A3471428D")]
 
-[assembly: AssemblyVersion("1.0.5.*")]
+[assembly: AssemblyVersion("1.0.6.*")]
 
 [assembly: InternalsVisibleTo("MultiPrecisionComplexAlgebraTest")]
