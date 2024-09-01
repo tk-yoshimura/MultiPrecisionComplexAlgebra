c8de3aebd71ff8b5a2022164ad404c319a107f76
tk-yoshimura <yoshimura8192@gmail.com>
2024-09-01 16:10:48 +0900

    fix: test svd

--- MultiPrecisionComplexAlgebraTests/Matrix/MatrixTests_SVD.cs
+++ MultiPrecisionComplexAlgebraTests/Matrix/MatrixTests_SVD.cs
@@ -1,4 +1,5 @@
 ﻿using MultiPrecision;
+using MultiPrecisionAlgebra;
 using MultiPrecisionComplex;
 using MultiPrecisionComplexAlgebra;
 
@@ -80,6 +81,58 @@ namespace MultiPrecisionComplexAlgebraTests {
             Assert.IsTrue((vv - ComplexMatrix<Pow2.N4>.Identity(5)).Det.Norm < 1e-30);
 
             Assert.IsTrue(MultiPrecision<Pow2.N4>.Abs((m - a).Norm) < 1e-30);
+        }
+
+        [TestMethod()]
+        public void SVDTest() {
+            for (int rows = 1; rows <= 8; rows++) {
+                for (int cols = 1; cols <= 8; cols++) {
+                    if (int.Abs(rows - cols) > 2) {
+                        continue;
+                    }
+
+                    Console.WriteLine($"{rows}x{cols}");
+
+                    for (int i = 0; i < 4; i++) {
+                        ComplexMatrix<Pow2.N4> m = TestCase<Pow2.N4>.RandomMatrix(rows, cols);
+
+                        Console.WriteLine(m);
+
+                        (ComplexMatrix<Pow2.N4> u, ComplexVector<Pow2.N4> s, ComplexMatrix<Pow2.N4> v) = ComplexMatrix<Pow2.N4>.SVD(m);
+
+                        ComplexMatrix<Pow2.N4> uu = u * u.H;
+                        ComplexMatrix<Pow2.N4> vv = v * v.H;
+
+                        ComplexMatrix<Pow2.N4> sm;
+
+                        if (rows == cols) {
+                            sm = ComplexMatrix<Pow2.N4>.FromDiagonals(s);
+                        }
+                        else if (rows < cols) {
+                            sm = ComplexMatrix<Pow2.N4>.HConcat(ComplexMatrix<Pow2.N4>.FromDiagonals(s), ComplexMatrix<Pow2.N4>.Zero(rows, cols - rows));
+                        }
+                        else {
+                            sm = ComplexMatrix<Pow2.N4>.VConcat(ComplexMatrix<Pow2.N4>.FromDiagonals(s), ComplexMatrix<Pow2.N4>.Zero(rows - cols, cols));
+                        }
+
+                        ComplexMatrix<Pow2.N4> a = u * sm * v.H;
+
+                        Console.WriteLine(uu);
+                        Console.WriteLine(vv);
+                        Console.WriteLine(a);
+
+                        Assert.IsTrue((u.Det.Norm - 1) < 1e-30);
+                        Assert.IsTrue((uu - ComplexMatrix<Pow2.N4>.Identity(rows)).Det.Norm < 1e-30);
+
+                        Assert.IsTrue((v.Det.Norm - 1) < 1e-30);
+                        Assert.IsTrue((vv - ComplexMatrix<Pow2.N4>.Identity(cols)).Det.Norm < 1e-30);
+
+                        Assert.IsTrue(MultiPrecision<Pow2.N4>.Abs((m - a).Norm) < 1e-30);
+
+                        Console.WriteLine("");
+                    }
+                }
+            }
         }
     }
 }
