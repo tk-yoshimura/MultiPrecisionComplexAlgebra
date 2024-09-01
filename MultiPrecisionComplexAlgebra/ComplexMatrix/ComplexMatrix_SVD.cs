a5ab946c81d545c26083d886676230ad42c21b68
tk-yoshimura <yoshimura8192@gmail.com>
2024-09-01 15:56:34 +0900

    add: svd

---
+++ MultiPrecisionComplexAlgebra/ComplexMatrix/ComplexMatrix_SVD.cs
@@ -0,0 +1,45 @@
+﻿using MultiPrecision;
+using MultiPrecisionComplex;
+using System.Diagnostics;
+using System.Linq;
+
+namespace MultiPrecisionComplexAlgebra {
+    public partial class ComplexMatrix<N> where N : struct, IConstant {
+        public static (ComplexMatrix<N> u, ComplexVector<N> s, ComplexMatrix<N> v) SVD(ComplexMatrix<N> m) {
+            if (m.Rows == m.Columns) {
+                (ComplexVector<N> eigen_vals, ComplexVector<N>[] eigen_vecs) = EigenValueVectors(m * m.H);
+
+                ComplexMatrix<N> u = HConcat(eigen_vecs);
+                ComplexVector<N> s = (Complex<N>.Sqrt, eigen_vals);
+                ComplexMatrix<N> v = (FromDiagonals(1 / s) * u.H * m).H;
+
+                return (u, s, v);
+            }
+            
+            if (m.Rows > m.Columns) {
+                (ComplexVector<N> eigen_vals, ComplexVector<N>[] eigen_vecs) = EigenValueVectors(m * m.H);
+
+                ComplexMatrix<N> u = HConcat(eigen_vecs);
+                ComplexVector<N> s = (Complex<N>.Sqrt, eigen_vals);
+                ComplexMatrix<N> v = (FromDiagonals(1 / s) * u.H * m).H;
+
+                s = s[..m.Columns];
+                v = v[.., ..m.Columns];
+
+                return (u, s, v);
+            }
+            else { 
+                (ComplexVector<N> eigen_vals, ComplexVector<N>[] eigen_vecs) = EigenValueVectors(m.H * m);
+
+                ComplexMatrix<N> v = HConcat(eigen_vecs);
+                ComplexVector<N> s = (Complex<N>.Sqrt, eigen_vals);
+                ComplexMatrix<N> u = m * v * FromDiagonals(1 / s);
+
+                s = s[..m.Rows];
+                u = u[.., ..m.Rows];
+
+                return (u, s, v);
+            }
+        }
+    }
+}
---
+++ MultiPrecisionComplexAlgebraTests/Matrix/MatrixTests_SVD.cs
@@ -0,0 +1,85 @@
+﻿using MultiPrecision;
+using MultiPrecisionComplex;
+using MultiPrecisionComplexAlgebra;
+
+namespace MultiPrecisionComplexAlgebraTests {
+    public partial class ComplexMatrixTests {
+        [TestMethod()]
+        public void SVD4x4Test() {
+            ComplexMatrix<Pow2.N4> m = new(new Complex<Pow2.N4>[,]
+                {{ ( 1, +2), ( 3, -1), ( 5, +0), ( 4, +2) },
+                 { ( 4, +0), ( 6, +2), ( 8, -3), ( 3, +1) },
+                 { ( 7, -1), ( 9, +1), (11, -2), (12, -3) },
+                 { ( 6, -2), ( 4, -1), ( 7,  1), ( 0,  3) } }
+            );
+
+            (ComplexMatrix<Pow2.N4> u, ComplexVector<Pow2.N4> s, ComplexMatrix<Pow2.N4> v) = ComplexMatrix<Pow2.N4>.SVD(m);
+
+            ComplexMatrix<Pow2.N4> uu = u * u.H;
+            ComplexMatrix<Pow2.N4> vv = v * v.H;
+
+            ComplexMatrix<Pow2.N4> a = u * ComplexMatrix<Pow2.N4>.FromDiagonals(s) * v.H;
+
+            Assert.IsTrue((u.Det.Norm - 1) < 1e-30);
+            Assert.IsTrue((uu - ComplexMatrix<Pow2.N4>.Identity(4)).Det.Norm < 1e-30);
+
+            Assert.IsTrue((v.Det.Norm - 1) < 1e-30);
+            Assert.IsTrue((vv - ComplexMatrix<Pow2.N4>.Identity(4)).Det.Norm < 1e-30);
+
+            Assert.IsTrue(MultiPrecision<Pow2.N4>.Abs((m - a).Norm) < 1e-30);
+        }
+
+        [TestMethod()]
+        public void SVD5x4Test() {
+            ComplexMatrix<Pow2.N4> m = new(new Complex<Pow2.N4>[,]
+                {{ ( 1, +2), ( 3, -1), ( 5, +0), ( 4, +2) },
+                 { ( 4, +0), ( 6, +2), ( 8, -3), ( 3, +1) },
+                 { ( 7, -1), ( 9, +1), (11, -2), (12, -3) },
+                 { (10, +0), (12, -1), (14, +2), ( 7, -4) }, 
+                 { ( 6, -2), ( 4, -1), ( 7,  1), ( 0,  3) } }
+            );
+
+            (ComplexMatrix<Pow2.N4> u, ComplexVector<Pow2.N4> s, ComplexMatrix<Pow2.N4> v) = ComplexMatrix<Pow2.N4>.SVD(m);
+
+            ComplexMatrix<Pow2.N4> uu = u * u.H;
+            ComplexMatrix<Pow2.N4> vv = v * v.H;
+
+            ComplexMatrix<Pow2.N4> sm = ComplexMatrix<Pow2.N4>.VConcat(ComplexMatrix<Pow2.N4>.FromDiagonals(s), ComplexMatrix<Pow2.N4>.Zero(1, 4));
+            ComplexMatrix<Pow2.N4> a = u * sm * v.H;
+
+            Assert.IsTrue((u.Det.Norm - 1) < 1e-30);
+            Assert.IsTrue((uu - ComplexMatrix<Pow2.N4>.Identity(5)).Det.Norm < 1e-30);
+
+            Assert.IsTrue((v.Det.Norm - 1) < 1e-30);
+            Assert.IsTrue((vv - ComplexMatrix<Pow2.N4>.Identity(4)).Det.Norm < 1e-30);
+
+            Assert.IsTrue(MultiPrecision<Pow2.N4>.Abs((m - a).Norm) < 1e-30);
+        }
+
+        [TestMethod()]
+        public void SVD4x5Test() {
+            ComplexMatrix<Pow2.N4> m = new(new Complex<Pow2.N4>[,]
+                {{ ( 1, +2), ( 3, -1), ( 5, +0), ( 4, +2), (6, -2) },
+                 { ( 4, +0), ( 6, +2), ( 8, -3), ( 3, +1), (4, -1) },
+                 { ( 7, -1), ( 9, +1), (11, -2), (12, -3), (7,  1) },
+                 { (10, +0), (12, -1), (14, +2), ( 7, -4), (0,  3) } }
+            );
+
+            (ComplexMatrix<Pow2.N4> u, ComplexVector<Pow2.N4> s, ComplexMatrix<Pow2.N4> v) = ComplexMatrix<Pow2.N4>.SVD(m);
+
+            ComplexMatrix<Pow2.N4> uu = u * u.H;
+            ComplexMatrix<Pow2.N4> vv = v * v.H;
+
+            ComplexMatrix<Pow2.N4> sm = ComplexMatrix<Pow2.N4>.HConcat(ComplexMatrix<Pow2.N4>.FromDiagonals(s), ComplexMatrix<Pow2.N4>.Zero(4, 1));
+            ComplexMatrix<Pow2.N4> a = u * sm * v.H;
+
+            Assert.IsTrue((u.Det.Norm - 1) < 1e-30);
+            Assert.IsTrue((uu - ComplexMatrix<Pow2.N4>.Identity(4)).Det.Norm < 1e-30);
+
+            Assert.IsTrue((v.Det.Norm - 1) < 1e-30);
+            Assert.IsTrue((vv - ComplexMatrix<Pow2.N4>.Identity(5)).Det.Norm < 1e-30);
+
+            Assert.IsTrue(MultiPrecision<Pow2.N4>.Abs((m - a).Norm) < 1e-30);
+        }
+    }
+}
